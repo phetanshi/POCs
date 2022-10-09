a93ae96b3d726c42d0d631da3497fc176a30628b
phetanshi <padmasekhar.pottepalem@outlook.com>
2022-10-09 21:58:19 +0530

    changes

--- PS.Database/PS.Database/dbo/Stored Procedures/uspGetEmployeeById.sql
+++ PS.Database/PS.Database/dbo/Stored Procedures/uspGetEmployeeById.sql
@@ -1,7 +1,7 @@
-﻿
-CREATE PROC [dbo].[uspGetEmployeeById]
-@EmployeeId INT
-AS
-BEGIN
-	SELECT * FROM tblEmployees WHERE ID = @EmployeeId
+﻿  
+CREATE PROC [dbo].[uspGetEmployeeById]  
+@EmployeeId INT  
+AS  
+BEGIN  
+ SELECT ID, FIRST_NAME, LAST_NAME, SALARY FROM tblEmployees WHERE ID = @EmployeeId  
 END
---
+++ PS.Database/PS.Database/dbo/Tables/tblDepartments.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblDepartments]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [DeptName] NVARCHAR(50) NULL, 
+    [IsActive] BIT NULL
+)
